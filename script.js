670afa37a1458dc349e1983ffe1b526ef4190c2f
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-02-27 19:31:42 -0600

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function scale(element, value) {
     element.style.transform = "scale(" + value + ")";
 }
+
+function mobileNav() {
+    var x = document.getElementById("mobile-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
