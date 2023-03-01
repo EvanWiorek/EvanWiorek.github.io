2d0c8357337f6f193b3366689ed0a35788f49c82
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-03-01 11:39:26 -0600

    update-video

--- script.js
+++ script.js
@@ -77,3 +77,15 @@ function carousel() {
     }
     setTimeout(carousel, 4000); // Change image every 2 seconds
 }
+
+
+//switch(expression) {
+    // case x:
+    //     // code block
+    //     break;
+    //   case y:
+    //     // code block
+    //     break;
+    //   default:
+    //     // code block
+    // }
